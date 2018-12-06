066c67ec89349fe9ac54ea7d0b0d102abd5c1593
Marianne Rachid <marianne.rachid@gmail.com>
2018-12-06 09:43:17 +0000

    Refactor #anagrams method
    Remove #binary_search and replaced it with built in ruby method bsearch

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,33 +4,20 @@ require 'uri'
 class Anagrams
   def initialize
     @wordlist = Net::HTTP.get(URI.parse('http://codekata.com/data/wordlist.txt')).split("\n")
+    @words = []
   end
 
   def anagrams(word)
-    words = permutations(word)
-    words.map! do |w|
-      binary_search(@wordlist, w)
+    word_permutations = permutations(word)
+    word_permutations.map! do |w|
+      search = @wordlist.bsearch { |value| w <=> value }
+      @words << search unless search.nil?
     end
-    words.delete(word)
-    puts words.compact
+    @words.delete(word)
+    puts @words
   end
 
   def permutations(word)
     word.split('').permutation.to_a.map!(&:join)
-  end
-
-  def binary_search(array, word)
-    low, high = 0, array.length - 1
-    while low <= high
-      mid = (low + high) >> 1
-      case word <=> array[mid]
-      when 1
-        low = mid + 1
-      when -1
-        high = mid - 1
-      else
-        return word
-      end
-    end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -9,14 +9,6 @@ describe Anagrams do
     end
   end
 
-  context '#binary-search' do
-    it 'tests binary search' do
-      array = ['odg', 'dgo', 'dog', 'ogd', 'gdo']
-      word = 'dog'
-      expect(subject.binary_search(array, word)).to eq('dog')
-    end
-  end
-
   context '#anagrams' do
     it 'outputs anagrams of word contained in wordlist' do
       expect { subject.anagrams('paste') }.to output("pates\npeats\nspate"\
