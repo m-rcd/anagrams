41bb8643436c0d9dcefedb156db0d40825d75ac4
Marianne Rachid <marianne.rachid@gmail.com>
2018-12-06 08:53:45 +0000

    Add #binary_search method and test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,4 +3,19 @@ class Anagrams
   def permutations(word)
     word.split('').permutation.to_a.map!(&:join)
   end
+
+  def binary_search(array, word)
+    low, high = 0, array.length - 1
+    while low <= high
+      mid = (low + high) >> 1
+      case word <=> array[mid]
+      when 1
+        low = mid + 1
+      when -1
+        high = mid - 1
+      else
+        return word
+      end
+    end  
+  end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,4 +8,12 @@ describe Anagrams do
       expect(subject.permutations('dog')).to eq(["dog", "dgo", "odg", "ogd", "gdo", "god"])
     end
   end
+
+  context '#binary-search' do
+    it 'tests binary search' do
+      array = ['odg', 'dgo', 'dog', 'ogd', 'gdo']
+      word = 'dog'
+      expect(subject.binary_search(array, word)).to eq('dog')
+    end
+  end
 end
